8e328f1907c1911ef8ae59f85ac42d10d0cfe533
zhz <2225986029@qq.com>
2018-11-29 21:15:11 +0800

    v1.0

--- utp发送数据.py
+++ utp发送数据.py
@@ -4,15 +4,16 @@ import socket
 
 def main():
 
+    #  AF_INET为IPV4协议，SOCK_DGRAM为udp通信协议
     utp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    utp_socket.bind(("", 6788))
-
-    win_address = ("192.168.130.1", 8080)
+    win_address = ("192.168.232.129", 6789)
 
     while True:
 
         send_input = input("请输入：")
+
+        # 发送数据，只能发送编码，解码为.decode
         utp_socket.sendto(send_input.encode("utf-8"), win_address)
 
         if send_input == 'exit':
--- utp接收数据.py
+++ utp接收数据.py
@@ -10,15 +10,16 @@ def main():
     print('等待接收')
     while True:
 
-        utp_recv = utp_socket.recvfrom(500)
+        # 接收数据
+        utp_recv = utp_socket.recvfrom(500)  #  指定最大接收字符串长度
 
         send_address = str(utp_recv[1][0]) + str(':') + str(utp_recv[1][1])
 
+        # 接收数据为byte类型，需解码
         send_message = (utp_recv[0]).decode('utf-8')
 
         if send_message == 'exit':
             print('退出聊天')
-            print('NB')
             exit()
 
         print("%s:%s" %(send_address, send_message))
