348d4ad6480b354023819556fb3a6630ef47851e
Zhang yuan <1414828626@qq.com>
2018-11-29 19:41:11 +0800

    add send and recve

---
+++ utp发送数据.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env.python
+#._*_ coding:utf-8 _*_
+import socket
+
+def main():
+
+    utp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    utp_socket.bind(("", 6788))
+
+    win_address = ("192.168.130.1", 8080)
+
+    while True:
+
+        send_input = input("请输入：")
+        utp_socket.sendto(send_input.encode("utf-8"), win_address)
+
+        if send_input == 'exit':
+            exit()
+
+    utp_socket.close()
+
+main()
+    
---
+++ utp接收数据.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env.python
+#._*_ coding:utf-8 _*_
+import socket
+
+def main():
+    utp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    utp_socket.bind(('',6789))
+
+    print('等待接收')
+    while True:
+
+        utp_recv = utp_socket.recvfrom(500)
+
+        send_address = str(utp_recv[1][0]) + str(':') + str(utp_recv[1][1])
+
+        send_message = (utp_recv[0]).decode('utf-8')
+
+        if send_message == 'exit':
+            exit()
+
+        print("%s:%s" %(send_address, send_message))
+
+    utp_socket.close()
+
+main()
