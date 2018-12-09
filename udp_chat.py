d05bbd9f2a0e96ae659f68ba8c5ab3c9bc79b43e
zhz <2225986029@qq.com>
2018-12-09 21:00:57 +0800

    n

--- udp_chat.py
+++ udp_chat.py
@@ -1,39 +1,6 @@
 #!/usr/bin/env.python
 #._*_ coding:utf-8 _*_
 import socket
-<<<<<<< HEAD
-
-class UdpChat(object):
-
-    def __init__(self):
-        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_socket.bind(("192.168.130.1",8081))
-
-    def chat(self):
-
-        while True:
-            self.send()
-            self.recve()
-
-    def send(self):
-        win_address = ("192.168.130.1", 8080)
-
-        send_message = input("Plese input the message to send:")
-
-        self.udp_socket.sendto(send_message.encode('utf-8'), win_address)
-
-    def recve(self):
-
-        recve_message = self.udp_socket.recvfrom(1024)
-        print(recve_message[0].decode('utf-8'))
-
-
-
-if __name__ == '__main__':
-    chat = UdpChat()
-
-    chat.chat()
-=======
 import threading
 
 class UdpChat(object):
@@ -68,4 +35,3 @@ if __name__ == '__main__':
 
     t_send.start()
     t_recve.start()
->>>>>>> dev
